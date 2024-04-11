2799bb6fd014622d62a1f7143a8f755a55fdfef1
Pranshu1902 <pranshu1902@gmail.com>
2024-04-11 22:56:14 +0530

    fetch just text from readme

--- api/readme.py
+++ api/readme.py
@@ -1,6 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
 
 username = "Kushagra102"
-res = requests.get(f"https://raw.githubusercontent.com/{username}/{username}/main/README.md")
 
-print(res._content)
+try:
+    res = requests.get(f"https://raw.githubusercontent.com/{username}/{username}/main/README.md")
+
+    if res.status_code == 200:
+        html_content = res.content
+        soup = BeautifulSoup(html_content, 'html.parser')
+        
+        # Extracting text from HTML
+        text = soup.get_text()
+        
+        print(text)
+    else:
+        print("Failed to fetch README")
+except Exception as e:
+    print("Error:", e)
